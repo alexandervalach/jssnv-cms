951e6a59c9a22c91ed060cca58330e0446c0c931
Jon Snow <Jon Snow@Lenovo-PC>
2015-09-01 15:45:50 +0200

    Vytvorenie komponenty a jej pridanie do základného repozitára.

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -2,14 +2,39 @@
 
 namespace App\Presenters;
 
+use App\Components\ModalDialog;
 use App\Model\PostRepository;
+use App\Model\SectionRepository;
+use App\Model\BannerRepository;
 use Nette\Application\UI\Presenter;
 
 /**
  * Base presenter for all application presenters.
  */
-abstract class BasePresenter extends Presenter
-{
-    /** @var PostRepository @inject */
-    public $postRepository;
+abstract class BasePresenter extends Presenter {
+
+    /** @var PostRepository */
+    protected $postRepository;
+    
+    /** @var SectionRepository */
+    protected $sectionRepository;
+    
+    /** @var BannerRepository */
+    protected $bannerRepository;
+
+    public function __construct(PostRepository $postRepository, SectionRepository $sectionRepository, BannerRepository $bannerRepository) {
+        parent::__construct();
+        $this->postRepository = $postRepository;
+        $this->sectionRepository = $sectionRepository;
+        $this->bannerRepository = $bannerRepository;
+    }
+    
+    public function beforeRender() {
+        $this->template->sections = $this->sectionRepository->findAll();
+    }
+
+    protected function createComponentModalDialog() {
+        return new ModalDialog();
+    }
+
 }
