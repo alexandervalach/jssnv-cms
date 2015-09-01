e776cdd87ccc9fe2a8c43fa93bc2b55ce4864a16
Jon Snow <Jon Snow@Lenovo-PC>
2015-09-01 16:27:55 +0200

    Vykreslenie homepage.

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -8,7 +8,10 @@ namespace App\Presenters;
 class HomepagePresenter extends BasePresenter {
 
     public function renderDefault() {
+        $section = $this->sectionRepository->findByValue('name', 'Základné informácie')->fetch();
         $this->template->banner = $this->bannerRepository->findAll()->order("order");
+        $this->template->section = $section;
+        $this->template->post = $section->related('post');
     }
 
 }
