b2c2028859e78df16e7f9b5fae974137f0f61867
Jon Snow <Jon Snow@Lenovo-PC>
2015-09-01 16:37:17 +0200

    Ošetrenie zobrazovania príspevkov.

--- app/presenters/PostPresenter.php
+++ app/presenters/PostPresenter.php
@@ -4,20 +4,22 @@ namespace App\Presenters;
 
 use Nette\Database\Table\ActiveRow;
 
-class PostPresenter extends BasePresenter{
+class PostPresenter extends BasePresenter {
+
     /** @var ActiveRow */
     private $postRow;
-    
+
     /** @var ActiveRow */
     private $sectionRow;
-            
+
     public function actionShow($id) {
         $this->sectionRow = $this->sectionRepository->findById($id);
-        $this->postRow = $this->sectionRow->post;
+        $this->postRow = $this->sectionRow->related('post')->fetch();
     }
-    
+
     public function renderShow() {
         $this->template->section = $this->sectionRow;
         $this->template->post = $this->postRow;
     }
+
 }
