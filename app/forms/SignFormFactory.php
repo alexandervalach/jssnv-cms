62b2f317bd7bd8a1cc19bbffe9a9cfd288281f6f
Jon Snow <Jon Snow@Lenovo-PC>
2015-09-01 20:41:24 +0200

    Vytvorenie formulára na prihlásenie.

--- app/forms/SignFormFactory.php
+++ app/forms/SignFormFactory.php
@@ -2,57 +2,52 @@
 
 namespace App\Forms;
 
-use Nette,
-	Nette\Application\UI\Form,
-	Nette\Security\User;
+use App\FormHelper;
+use Nette;
+use Nette\Application\UI\Form;
+use Nette\Security\User;
 
+class SignFormFactory extends Nette\Object {
 
-class SignFormFactory extends Nette\Object
-{
-	/** @var User */
-	private $user;
+    /** @var User */
+    private $user;
 
+    public function __construct(User $user) {
+        $this->user = $user;
+    }
 
-	public function __construct(User $user)
-	{
-		$this->user = $user;
-	}
+    /**
+     * @return Form
+     */
+    public function create() {
+        $form = new Form;
+        $form->addText('username', 'Username:')
+                ->setRequired('Zadaj, prosím, užívateľské meno.');
 
+        $form->addPassword('password', 'Password:')
+                ->setRequired('Zadaj, prosím, heslo.');
 
-	/**
-	 * @return Form
-	 */
-	public function create()
-	{
-		$form = new Form;
-		$form->addText('username', 'Username:')
-			->setRequired('Please enter your username.');
+        $form->addCheckbox('remember', 'Trvalé prihlásenie');
 
-		$form->addPassword('password', 'Password:')
-			->setRequired('Please enter your password.');
+        $form->addSubmit('send', 'Sign in');
 
-		$form->addCheckbox('remember', 'Keep me signed in');
+        $form->onSuccess[] = array($this, 'formSucceeded');
+        FormHelper::setBootstrapRenderer($form);
+        return $form;
+    }
 
-		$form->addSubmit('send', 'Sign in');
+    public function formSucceeded($form, $values) {
+        if ($values->remember) {
+            $this->user->setExpiration('14 days', FALSE);
+        } else {
+            $this->user->setExpiration('20 minutes', TRUE);
+        }
 
-		$form->onSuccess[] = array($this, 'formSucceeded');
-		return $form;
-	}
-
-
-	public function formSucceeded($form, $values)
-	{
-		if ($values->remember) {
-			$this->user->setExpiration('14 days', FALSE);
-		} else {
-			$this->user->setExpiration('20 minutes', TRUE);
-		}
-
-		try {
-			$this->user->login($values->username, $values->password);
-		} catch (Nette\Security\AuthenticationException $e) {
-			$form->addError($e->getMessage());
-		}
-	}
+        try {
+            $this->user->login($values->username, $values->password);
+        } catch (Nette\Security\AuthenticationException $e) {
+            $form->addError($e->getMessage());
+        }
+    }
 
 }
--- app/presenters/SignPresenter.php
+++ app/presenters/SignPresenter.php
@@ -2,38 +2,33 @@
 
 namespace App\Presenters;
 
-use Nette,
-	App\Forms\SignFormFactory;
-
+use Nette;
+use App\Forms\SignFormFactory;
 
 /**
  * Sign in/out presenters.
  */
-class SignPresenter extends BasePresenter
-{
-	/** @var SignFormFactory @inject */
-	public $factory;
+class SignPresenter extends BasePresenter {
 
+    /** @var SignFormFactory @inject */
+    public $factory;
 
-	/**
-	 * Sign-in form factory.
-	 * @return Nette\Application\UI\Form
-	 */
-	protected function createComponentSignInForm()
-	{
-		$form = $this->factory->create();
-		$form->onSuccess[] = function ($form) {
-			$form->getPresenter()->redirect('Homepage:');
-		};
-		return $form;
-	}
+    /**
+     * Sign-in form factory.
+     * @return Nette\Application\UI\Form
+     */
+    protected function createComponentSignInForm() {
+        $form = $this->factory->create();
+        $form->onSuccess[] = function ($form) {
+            $form->getPresenter()->redirect('Homepage:');
+        };
+        return $form;
+    }
 
-
-	public function actionOut()
-	{
-		$this->getUser()->logout();
-		$this->flashMessage('You have been signed out.');
-		$this->redirect('in');
-	}
+    public function actionOut() {
+        $this->getUser()->logout();
+        $this->flashMessage('You have been signed out.');
+        $this->redirect('in');
+    }
 
 }
