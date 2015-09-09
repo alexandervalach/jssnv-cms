64298aa3a00e019d8c5b9398300df5b53e005c14
Jon Snow <Jon Snow@Lenovo-PC>
2015-09-09 14:10:30 +0200

    Nový postPresenter.

--- app/presenters/PostPresenter.php
+++ app/presenters/PostPresenter.php
@@ -2,7 +2,10 @@
 
 namespace App\Presenters;
 
+use App\FormHelper;
 use Nette\Database\Table\ActiveRow;
+use Nette\Application\BadRequestException;
+use Nette\Application\UI\Form;
 
 class PostPresenter extends BasePresenter {
 
@@ -11,6 +14,9 @@ class PostPresenter extends BasePresenter {
 
     /** @var ActiveRow */
     private $sectionRow;
+
+    /** @var string */
+    private $error = "Post not found!";
 
     public function actionShow($id) {
         $this->sectionRow = $this->sectionRepository->findById($id);
@@ -21,5 +27,47 @@ class PostPresenter extends BasePresenter {
         $this->template->section = $this->sectionRow;
         $this->template->post = $this->postRow;
     }
+
+    public function actionEdit($id) {
+        $this->userIsLogged();
+        $this->sectionRow = $this->sectionRepository->findById($id);
+        $this->postRow = $this->sectionRow->related('post')->fetch();
+    }
+
+    public function renderEdit($id) {
+        $this->sectionRow;
+        if (!$this->sectionRow) {
+            throw new BadRequestException($this->error);
+        }
+        $this->template->post = $this->postRow;
+        $this->template->section = $this->sectionRow;
+        $this->getComponent('editForm')->setDefaults($this->postRow);
+    }
+
+    protected function createComponentEditForm() {
+        $form = new Form;
+
+        $form->addTextArea('content', 'Obsah:')
+                ->setAttribute('class', 'form-jqte')
+                ->setRequired("Obsah príspevku je povinné pole.");
+
+        $form->addSubmit('save', 'Uložiť');
+
+        $form->onSuccess[] = $this->submittedEditForm;
+        FormHelper::setBootstrapRenderer($form);
+        return $form;
+    }
+
+    public function submittedEditForm(Form $form) {
+        $this->userIsLogged();
+        $values = $form->getValues();
+        $this->postRow->update($values);
+        $this->redirect('show', $this->postRow->id);
+    }
+    /*
+    public function formCancelled() {
+        $this->redirect('Homepage:');
+    }
+     */
 
 }
