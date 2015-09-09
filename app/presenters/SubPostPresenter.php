52e5d69dd0c17dc094c9b8a79eb7b3c5721b966a
Jon Snow <Jon Snow@Lenovo-PC>
2015-09-09 21:41:20 +0200

    Prezentre.

---
+++ app/presenters/SubPostPresenter.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Presenters;
+
+use App\FormHelper;
+use Nette\Application\BadRequestException;
+use Nette\Application\UI\Form;
+use Nette\Database\Table\ActiveRow;
+
+class SubPostPresenter extends BasePresenter {
+
+    /** @var ActiveRow */
+    private $subPostRow;
+
+    /** @var ActiveRow */
+    private $subSectionRow;
+
+    /** @var string */
+    private $error =
+
+    "Post not found!";
+
+    public function actionShow($id) {
+            $this->subSectionRow = $this->subSectionRepository->findById($id);
+            $this->subPostRow = $this->subPostRepository->findByValue( 'subsection_id', $id)->fetch();
+    }
+
+    public function renderShow($id) {
+            $this->template->subSection = $this->subSectionRow;
+            $this->template->subPost = $this->subPostRow;
+    }
+
+    public function actionEdit($id) {
+    $this->userIsLogged();
+            $this->subSectionRow = $this->subSectionRepository->findById($id);
+            $this->subPostRow = $this->subSectionRow->related('subpost')->fetch();
+    }
+
+    public function renderEdit($id) {
+            $this->subSectionRow;
+    if (!$this->subSectionRow) {
+    throw new BadRequestException($this->error);
+    }
+    if (!$this->subPostRow) {
+    throw new BadRequestException($this->error);
+    }
+            $this->template->subPost = $this->subPostRow;
+            $this->template->subSection = $this->subSectionRow;
+    $this->getComponent('editForm')->setDefaults($this->subPostRow);
+    }
+
+    protected function createComponentEditForm() {
+            $form = new Form;
+
+            $form->addTextArea( 'content', 'Obsah:')->setAttribute( 'class', 'form-jqte')->setRequired("Obsah príspevku je povinné pole.");
+
+    $form->addSubmit( 'save', 'Uložiť');
+
+            $form->onSuccess[] = $this->submittedEditForm;
+    FormHelper::setBootstrapRenderer($form);
+    return $form;
+    }
+
+    public function submittedEditForm(Form $form) {
+    $this->userIsLogged();
+            $values = $form->getValues();
+    $this->subPostRow->update($values);
+    $this->redirect( 'show', $this->subPostRow->id);
+}
+
+}
---
+++ app/presenters/SubSectionPresenter.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Presenters;
+
+use App\FormHelper;
+use Nette\Application\UI\Form;
+use Nette\Database\Table\ActiveRow;
+
+class SubSectionPresenter extends BasePresenter {
+
+    /** @var ActiveRow */
+    private $subSectionRow;
+
+    public function actionEdit($id) {
+        $this->subSectionRow = $this->subSectionRepository->findById($id);
+    }
+
+    public function renderEdit($id) {
+        if (!$this->subSectionRow) {
+            throw new BadRequestException($this->error);
+        }
+        $this->template->subSection = $this->subSectionRow;
+        $this->getComponent('editForm')->setDefaults($this->subSectionRow);
+    }
+
+    public function actionAdd() {
+    }
+
+    public function renderAdd() {
+        $this->getComponent('addForm');
+    }
+
+    protected function createComponentAddForm() {
+        $form = new Form;
+        $form->addText('name', 'Názov')
+                ->setRequired('Názov musí byť vyplnený.')
+                ->addRule(Form::MAX_LENGTH, 'Názov môže mať maximálne 50 znakov.', 50);
+        $form->addText('link', 'Link')
+                ->addRule(Form::MAX_LENGTH, 'Link môže mať maximálne 255 znakov', 255);
+        $form->addSubmit('save', 'Zapísať');
+
+        $form->onSuccess[] = $this->submittedAddForm;
+        FormHelper::setBootstrapRenderer($form);
+        return $form;
+    }
+
+    public function submittedAddForm(Form $form) {
+        $values = $form->getValues();
+        $id = $this->subSectionRepository->insert($values);
+
+        if ($values['link'] != '') {
+            $postData = array(
+                'section_id' => $id,
+            );
+            $this->subPostRepository->insert($postData);
+            $this->redirect('SubPost:show', $id);
+        }
+        $this->redirect('Homepage:');
+    }
+
+    protected function createComponentEditForm() {
+        $form = new Form;
+        $form->addText('name', 'Názov')
+                ->setRequired('Názov musí byť vyplnený.')
+                ->addRule(Form::MAX_LENGTH, 'Názov môže mať maximálne 50 znakov.', 50);
+        $form->addText('link', 'Link')
+                ->addRule(Form::MAX_LENGTH, 'Link môže mať maximálne 255 znakov', 255);
+        $form->addSubmit('save', 'Zapísať');
+
+        $form->onSuccess[] = $this->submittedEditForm;
+        FormHelper::setBootstrapRenderer($form);
+        return $form;
+    }
+
+    public function submittedEditForm(Form $form) {
+        $values = $form->getValues();
+        $this->subSectionRow->update($values);
+        $this->redirect('SubPost:show', $this->subSectionRow->id);
+    }
+
+    public function submittedDeleteForm() {
+        
+    }
+}
