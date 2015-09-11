d94b067aa423a30dc5fe9b0c6ea0203d4b358d2b
Jon Snow <Jon Snow@Lenovo-PC>
2015-09-11 22:15:50 +0200

    Úlohy užívateľov.

--- app/model/UserRepository.php
+++ app/model/UserRepository.php
@@ -3,5 +3,9 @@
 namespace App\Model;
 
 class UserRepository extends Repository{
-    
+    public static $ROLES = array(
+        '1' => 'admin',
+        '2' => 'user',
+        '3' => 'powerUser'
+    );
 }
