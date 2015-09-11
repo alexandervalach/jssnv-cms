dccdde27b8282f3d6c480781e7a337bbc09dbb66
Jon Snow <Jon Snow@Lenovo-PC>
2015-09-11 10:55:15 +0200

    Pridávanie oznamov.

--- app/model/NoticeRepository.php
+++ app/model/NoticeRepository.php
@@ -3,5 +3,12 @@
 namespace App\Model;
 
 class NoticeRepository extends Repository {
-    
+
+    public static $flag = array(
+        'info' => 'informácia',
+        'success' => 'obyčajný oznam',
+        'warning' => 'upozornenie',
+        'danger' => 'dôležité'
+    );
+
 }
