7b5ee5719ab357a50e64d4ea33c09bf9b2caf539
Jon Snow <Jon Snow@Lenovo-PC>
2015-09-19 18:04:49 +0200

    CSS, Prezenter a šablóna na zobrazovanie zvolených noviniek.

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -9,6 +9,8 @@ class HomepagePresenter extends BasePresenter {
 
     public function renderDefault() {
         $this->template->bannerItems = $this->bannerRepository->findAll();
+        $this->template->posts = $this->postRepository->findByValue('onHomepage', 1);
+        $this->template->subPosts = $this->subPostRepository->findByValue('onHomepage', 1);
     }
 
 }
