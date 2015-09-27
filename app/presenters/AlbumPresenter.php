d4a27e13282f9e75dbc10a946e827808a6464a2a
Jon Snow <Jon Snow@Lenovo-PC>
2015-09-27 10:50:44 +0200

    Pridanie albumov.

--- app/presenters/AlbumPresenter.php
+++ app/presenters/AlbumPresenter.php
@@ -2,6 +2,73 @@
 
 namespace App\Presenters;
 
+use App\FormHelper;
+use Nette\Application\BadRequestException;
+use Nette\Application\UI\Form;
+use Nette\Database\Table\ActiveRow;
+
 class AlbumPresenter extends BasePresenter {
-    //put your code here
+
+    /** @var ActiveRow */
+    private $albumRow;
+
+    /** @var string */
+    private $error = "Album not found!";
+
+    public function actionEdit($id) {
+        $this->albumRow = $this->albumRepository->findById($id);
+    }
+
+    public function renderEdit($id) {
+        if (!$this->albumRow) {
+            throw new BadRequestException($this->error);
+        }
+        $this->template->album = $this->albumRow;
+        $this->getComponent('editForm')->setDefaults($this->albumRow);
+    }
+
+    public function actionAdd() {
+        $this->userIsLogged();
+    }
+
+    public function renderAdd() {
+        $this->getComponent('addForm');
+    }
+
+    protected function createComponentAddForm() {
+        $form = new Form;
+        $form->addText('name', 'Názov')
+                ->setRequired('Názov musí byť vyplnený.')
+                ->addRule(Form::MAX_LENGTH, 'Názov môže mať maximálne 50 znakov.', 50);
+        $form->addSubmit('save', 'Zapísať');
+
+        $form->onSuccess[] = $this->submittedAddForm;
+        FormHelper::setBootstrapRenderer($form);
+        return $form;
+    }
+
+    public function submittedAddForm(Form $form) {
+        $values = $form->getValues();
+        $id = $this->albumRepository->insert($values);
+        $this->redirect('Gallery:view', $id);
+    }
+
+    protected function createComponentEditForm() {
+        $form = new Form;
+        $form->addText('name', 'Názov')
+                ->setRequired('Názov musí byť vyplnený.')
+                ->addRule(Form::MAX_LENGTH, 'Názov môže mať maximálne 50 znakov.', 50);
+        $form->addSubmit('save', 'Zapísať');
+
+        $form->onSuccess[] = $this->submittedEditForm;
+        FormHelper::setBootstrapRenderer($form);
+        return $form;
+    }
+
+    public function submittedEditForm(Form $form) {
+        $values = $form->getValues();
+        $this->albumRow->update($values);
+        $this->redirect('Gallery:view', $this->albumRow->id);
+    }
+
 }
