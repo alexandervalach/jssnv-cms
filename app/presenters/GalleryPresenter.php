23ffe4bf1149e5eabc76445b7c62f7301a427f12
Jon Snow <Jon Snow@Lenovo-PC>
2015-09-27 10:51:39 +0200

    Vytvorenie náhľadu do galérie.

--- app/presenters/GalleryPresenter.php
+++ app/presenters/GalleryPresenter.php
@@ -2,6 +2,28 @@
 
 namespace App\Presenters;
 
+use Nette\Database\Table\ActiveRow;
+use Nette\Database\Table\Selection;
+
 class GalleryPresenter extends BasePresenter {
-    //put your code here
+
+    /** @var Selection */
+    private $gallerySelection;
+
+    /** @var string */
+    private $error = "Gallery not found!";
+
+    public function actionView($id) {
+        $this->gallerySelection = $this->galleryRepository->findByValue('album_id', $id);
+    }
+
+    public function renderView($id) {
+        if (!$this->gallerySelection) {
+            $this->template->images = null;
+        } else {
+            $this->template->images = $this->galleryRepository->findByValue('album_id', $id);
+        }
+        $this->template->imgFolder = $this->imgFolder;
+    }
+
 }
