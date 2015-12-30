7de8bd6c3a6c38da7f52095eb20c4d24f361c3a1
jonsnow <jonsnow@jonsnow-Lenovo-B590>
2015-12-30 19:35:44 +0100

    Zmena v komponente FormCancelled.

--- app/presenters/templates/components/FormCancelled.php
+++ app/presenters/templates/components/FormCancelled.php
@@ -6,11 +6,12 @@ use Nette\Application\UI\Control;
 
 class FormCancelled extends Control {
 
-    public function render($link, $text, $icon = null) {
+    public function render($link, $text, $class, $icon = null) {
         $this->template->setFile(__DIR__ . '/FormCancelled.latte');
         $this->template->link = $link;
-        $this->template->icon = $icon;
         $this->template->text = $text;
+        $this->template->class = $class;
+        $this->template->icon = $icon;
         $this->template->render();
     }
 
