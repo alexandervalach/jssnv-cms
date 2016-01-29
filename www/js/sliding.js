2aa37bbf5a1ea65c004105077c679d3abc69c395
jonsnow <jonsnow@jonsnow-Lenovo-B590>
2016-01-29 17:03:15 +0100

    Úprava jQuery rolovacieho efektu.

--- www/js/sliding.js
+++ www/js/sliding.js
@@ -3,10 +3,12 @@
 //$('.slideToggle').find('ul').slideUp();
 
 $(document).ready(function(){
-    // $('li.slideToggle').bind('click', function);
+    $('li.slideToggle').each(function (){
+        $(this).find('ul').css("display", "none");
+    });
     
     $('li.slideToggle').bind('click', function(){
-        $(this).find('ul').slideToggle();
+        $(this).find('ul').not(':animated').slideToggle();
         return false;
     });
     $('li.slideToggle ul').bind('click', function(e){
