7069c7d877ce7c840a3de0780c29d6db360b61d8
jonsnow <jonsnow@jonsnow-Lenovo-B590>
2016-02-09 12:46:09 +0100

    Pridanie akcie na odstránenie oznamov.

--- app/presenters/NoticePresenter.php
+++ app/presenters/NoticePresenter.php
@@ -7,11 +7,15 @@ use App\Model\NoticeRepository;
 use Nette\Application\UI\Form;
 use Nette\Database\Table\ActiveRow;
 use Nette\Forms\Controls\SubmitButton;
+use Nette\Application\BadRequestException;
 
 class NoticePresenter extends BasePresenter {
 
     /** @var ActiveRow */
     private $noticeRow;
+
+    /** @var string */
+    private $error = "Notice not found!";
 
     public function actionAll() {
         
@@ -39,6 +43,19 @@ class NoticePresenter extends BasePresenter {
         $this->getComponent('editForm')->setDefaults($this->noticeRow);
     }
 
+    public function actionRemove($id) {
+        $this->userIsLogged();
+        $this->noticeRow = $this->noticeRepository->findById($id);
+    }
+
+    public function renderRemove($id) {
+        if (!$this->noticeRow) {
+            throw new BadRequestException($this->error);
+        }
+        $this->template->notice = $this->noticeRow;
+        $this->getComponent('removeForm');
+    }
+
     protected function createComponentAddForm() {
         $form = new Form;
         $form->addSelect('type', 'Typ', NoticeRepository::$flag);
@@ -62,9 +79,20 @@ class NoticePresenter extends BasePresenter {
         $form->addSubmit('save', 'Zapísať')
                 ->onClick[] = $this->submittedEditForm;
         $form->addSubmit('cancel', 'Zrušiť')
-                ->setAttribute('class', 'btn btn-warning')
+                        ->setAttribute('class', 'btn btn-warning')
                 ->onClick[] = $this->formCancelled;
         FormHelper::setBootstrapRenderer($form);
+        return $form;
+    }
+
+    protected function createComponentRemoveForm() {
+        $form = new Form;
+        $form->addSubmit('remove', 'Odstrániť')
+                        ->setAttribute('class', 'btn btn-danger')
+                ->onClick[] = $this->submittedRemoveForm;
+        $form->addSubmit('cancel', 'Zrušiť')
+                        ->setAttribute('class', 'btn btn-warning')
+                ->onClick[] = $this->formCancelled;
         return $form;
     }
 
@@ -72,19 +100,20 @@ class NoticePresenter extends BasePresenter {
         $values = $form->getValues();
         $values['time'] = date('Y-m-d h:i:s');
         $this->noticeRepository->insert($values);
-        $this->redirect('all');
+        $this->redirect('all#primary');
     }
 
     public function submittedEditForm(SubmitButton $btn) {
         $values = $btn->form->getValues();
         $this->noticeRow->update($values);
-        $this->redirect('all');
+        $this->redirect('all#primary');
     }
 
-    public function submittedDeleteForm() {
-        
+    public function submittedRemoveForm() {
+        $this->noticeRow->delete();
+        $this->redirect('all#primary');
     }
-    
+
     public function formCancelled() {
         $this->redirect('all#primary');
     }
