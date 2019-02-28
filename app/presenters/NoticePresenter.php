16e1a0ea128f4952d3b63de237573893a84c2889
Alexander Valach <alexander.valach@gmail.com>
2019-02-28 09:50:15 +0100

    Fixed deprecation warnings for notice presenter

--- app/presenters/NoticePresenter.php
+++ app/presenters/NoticePresenter.php
@@ -17,9 +17,6 @@ class NoticePresenter extends BasePresenter {
     /** @var string */
     private $error = "Notice not found!";
 
-    public function actionAll() {
-        
-    }
 
     public function renderAll() {
         $this->template->notices = $this->noticeRepository->findAll()->order('time DESC');
@@ -29,13 +26,13 @@ class NoticePresenter extends BasePresenter {
         $this->userIsLogged();
     }
 
-    public function renderAdd() {
-        $this->getComponent('addForm');
-    }
-
     public function actionEdit($id) {
         $this->userIsLogged();
         $this->noticeRow = $this->noticeRepository->findById($id);
+
+        if (!$this->noticeRow) {
+            throw new BadRequestException($this->error);
+        }
     }
 
     public function renderEdit($id) {
@@ -46,12 +43,13 @@ class NoticePresenter extends BasePresenter {
     public function actionRemove($id) {
         $this->userIsLogged();
         $this->noticeRow = $this->noticeRepository->findById($id);
-    }
 
-    public function renderRemove($id) {
         if (!$this->noticeRow) {
             throw new BadRequestException($this->error);
         }
+    }
+
+    public function renderRemove($id) {
         $this->template->notice = $this->noticeRow;
         $this->getComponent('removeForm');
     }
@@ -64,7 +62,8 @@ class NoticePresenter extends BasePresenter {
         $form->addTextArea('content', 'Text')
                 ->setAttribute('id', 'ckeditor');
         $form->addSubmit('save', 'Zapísať');
-        $form->onSuccess[] = $this->submittedAddForm;
+
+        $form->onSuccess[] = [$this, 'submittedAddForm'];
         FormHelper::setBootstrapRenderer($form);
         return $form;
     }
@@ -72,44 +71,54 @@ class NoticePresenter extends BasePresenter {
     protected function createComponentEditForm() {
         $form = new Form;
         $form->addSelect('type', 'Typ', NoticeRepository::$flag);
+        
         $form->addText('name', 'Názov')
                 ->setRequired("Názov musí byť vyplnený");
+        
         $form->addTextArea('content', 'Text')
                 ->setAttribute('id', 'ckeditor');
+
         $form->addSubmit('save', 'Zapísať')
-                ->onClick[] = $this->submittedEditForm;
+                ->onClick[] = [$this, 'submittedEditForm'];
+        
         $form->addSubmit('cancel', 'Zrušiť')
                         ->setAttribute('class', 'btn btn-warning')
-                ->onClick[] = $this->formCancelled;
+                ->onClick[] = [$this, 'formCancelled'];
+        
         FormHelper::setBootstrapRenderer($form);
         return $form;
     }
 
     protected function createComponentRemoveForm() {
         $form = new Form;
+        
         $form->addSubmit('cancel', 'Zrušiť')
                         ->setAttribute('class', 'btn btn-warning')
-                ->onClick[] = $this->formCancelled;
+                ->onClick[] = [$this, 'formCancelled'];
         $form->addSubmit('remove', 'Odstrániť')
                         ->setAttribute('class', 'btn btn-danger')
-                ->onClick[] = $this->submittedRemoveForm;
+                ->onClick[] = [$this, 'submittedRemoveForm'];
+
+        FormHelper::setBootstrapRenderer($form);
         return $form;
     }
 
-    public function submittedAddForm(Form $form) {
-        $values = $form->getValues();
+    public function submittedAddForm(Form $form, $values) {
+        $this->userIsLogged();
         $values['time'] = date('Y-m-d h:i:s');
         $this->noticeRepository->insert($values);
         $this->redirect('all#primary');
     }
 
     public function submittedEditForm(SubmitButton $btn) {
+        $this->userIsLogged();
         $values = $btn->form->getValues();
         $this->noticeRow->update($values);
         $this->redirect('all#primary');
     }
 
     public function submittedRemoveForm() {
+        $this->userIsLogged();
         $this->noticeRow->delete();
         $this->redirect('all#primary');
     }
