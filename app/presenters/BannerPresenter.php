ea01bd409eada4d75930b31d49314ef736c77079
Alexander Valach <alexander.valach@gmail.com>
2019-02-28 10:53:27 +0100

    Removed deprecation warnings

--- app/presenters/BannerPresenter.php
+++ app/presenters/BannerPresenter.php
@@ -18,34 +18,39 @@ class BannerPresenter extends BasePresenter {
 
     public function actionEdit($id) {
         $this->bannerRow = $this->bannerRepository->findById($id);
-    }
 
-    public function renderEdit($id) {
         if (!$this->bannerRow) {
             throw new BadRequestException($this->error);
         }
+    }
+
+    public function renderEdit($id) {
         $this->template->banner = $this->bannerRow;
         $this->getComponent('editForm')->setDefaults($this->bannerRow);
     }
 
     protected function createComponentEditForm() {
         $form = new Form;
-        $form->addTextArea('message', 'Text:')
+
+        $form->addTextArea('message', 'Text')
                 ->addRule(Form::FILLED, 'Text muís byť vyplnený.')
                 ->addRule(Form::MAX_LENGTH, 'Maximálna dĺžka textu je 250 znakov.', 250);
-        $form->addText('link', 'Odkaz:')
-                ->addRule(Form::MAX_LENGTH, 'Maximálna dĺžka odkazu je 250 znakov.', 250);
+
+        $form->addText('link', 'Odkaz');
+        
         $form->addSubmit('save', 'Zapísať')
-                ->onClick[] = $this->submittedEditForm;
+                ->onClick[] = [$this, 'submittedEditForm'];
+        
         $form->addSubmit('cancel', 'Zrušiť')
                 ->setAttribute('class', 'btn btn-warning')
-                ->onClick[] = $this->formCancelled;
+                ->onClick[] = [$this, 'formCancelled'];
 
         FormHelper::setBootstrapRenderer($form);
         return $form;
     }
 
     public function submittedEditForm(SubmitButton $btn) {
+        $this->userIsLogged();
         $values = $btn->form->getValues();
         $this->bannerRow->update($values);
         $this->redirect('Homepage:#primary');
