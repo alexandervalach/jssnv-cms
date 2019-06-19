0672c1b7d92e180a4068b239e19f276eb58976d3
Alexander Valach <alexander.valach@gmail.com>
2019-06-19 13:37:38 +0200

    Possibility to add answers o question

---
+++ app/presenters/AnswersPresenter.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Presenters;
+
+use App\FormHelper;
+use Nette\Application\UI\Form;
+use Nette\Database\Table\ActiveRow;
+use Nette\Database\Table\Selection;
+
+namespace App\Presenters;
+
+class AnswersPresenter extends BasePresenter {
+
+  /** @var ActiveRow */
+  private $answerRow;
+
+  /** @var ActiveRow **/ 
+  private $questionRow;
+
+  /**
+   * @param id test id 
+   */
+  public function actionAll ($id) {
+    $this->userIsLogged();
+    $this->questionRow = $this->questionsRepository->findById($id);
+
+    if (!$this->questionRow) {
+      $this->error(self::ITEM_NOT_FOUND);
+    }
+  }
+
+  /**
+   * @param id test id
+   */
+  public function renderAll ($id) {
+    $this->template->question = $this->questionRow;
+    $this->template->answers = $this->questionRow->related('answers');
+  }
+
+  public function submittedAddForm ($form, $values) {
+    $this->answersRepository->insert($values);
+    $this->flashMessage(self::ITEM_ADD_SUCCESS);
+    $this->redirect('all', $this->questionRow); 
+  }
+
+  protected function createComponentAddForm () {
+    $form = new \Nette\Application\UI\Form;
+
+    $form->addText('label', 'Odpoveď');
+    $form->addHidden('question_id', $this->questionRow);
+    $form->addCheckbox('correct', ' Správna odpoveď');
+    $form->addSubmit('save', 'Uložiť');
+    $form->onSuccess[] = [$this, 'submittedAddForm'];
+
+    \App\FormHelper::setBootstrapRenderer($form);
+    return $form;
+  }
+}
