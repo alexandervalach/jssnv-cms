ca912649460b26bc371d550bd89991a0f85c7238
Alexander Valach <alexander.valach@gmail.com>
2019-06-19 14:49:09 +0200

    Added scroll to top button

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,23 @@
-$(function(){
+$(function() {
+  var scroll_btn = $('#scroll-to-top');
+  var body = $('html, body');
 
+  showScrollBtn(body, scroll_btn);
+  scrolToTop(body, scroll_btn);
 });
+
+function scrolToTop(body, scroll_btn) {
+  scroll_btn.bind('click', function() {
+    body.animate({ scrollTop: 0}, 'fast');
+  });
+}
+
+function showScrollBtn(body, scrollBtn) {
+  $(window).scroll( function() {
+    if (body.scrollTop() > 20) {
+      scrollBtn.fadeIn();
+    } else {
+      scrollBtn.fadeOut();
+    }
+  });
+}
