a1f59312827b05669284cdf27469225f7d653812
Alexander Valach <alexander.valach@gmail.com>
2020-06-08 20:35:55 +0200

    Added basic Course management, minor template changes

--- app/presenters/ApplicationFormsPresenter.php
+++ app/presenters/ApplicationFormsPresenter.php
@@ -1,10 +1,25 @@
 <?php
 
+declare(strict_types=1);
 
 namespace App\Presenters;
 
+use App\Components\BreadcrumbControl;
+use App\Forms\SearchFormFactory;
+use App\Model\AlbumsRepository;
+use App\Model\SectionsRepository;
 
 class ApplicationFormsPresenter extends BasePresenter
 {
+  public function __construct(AlbumsRepository $albumsRepository, SectionsRepository $sectionRepository, BreadcrumbControl $breadcrumbControl, SearchFormFactory $searchForm)
+  {
+    parent::__construct($albumsRepository, $sectionRepository, $breadcrumbControl, $searchForm);
+  }
 
+  public function actionAdd () {
+
+  }
+
+  public function renderAdd () {
+  }
 }
--- app/presenters/CoursesPresenter.php
+++ app/presenters/CoursesPresenter.php
@@ -1,9 +1,105 @@
 <?php
 
+declare(strict_types=1);
 
 namespace App\Presenters;
 
+use App\Components\BreadcrumbControl;
+use app\forms\CourseFormFactory;
+use App\Forms\SearchFormFactory;
+use App\Model\AlbumsRepository;
+use App\Model\CoursesRepository;
+use App\Model\SectionsRepository;
+use Nette\Application\AbortException;
+use Nette\Application\BadRequestException;
+use Nette\Application\UI\Form;
+use Nette\Database\Table\ActiveRow;
+use Nette\Utils\ArrayHash;
+
 class CoursesPresenter extends BasePresenter
 {
+  /**
+   * @var CourseFormFactory
+   */
+  private $courseFormFactory;
 
+  /**
+   * @var CoursesRepository
+   */
+  private $coursesRepository;
+
+  /** @var ActiveRow */
+  private $courseRow;
+
+  public function actionView (int $id): void
+  {
+    $this->courseRow = $this->coursesRepository->findById($id);
+
+    if (!$this->courseRow) {
+      throw new BadRequestException(self::ITEM_NOT_FOUND);
+    }
+
+    try {
+      $this->guestRedirect();
+      $this['breadcrumb']->add('Kurzy', 'Courses:all');
+      $this['breadcrumb']->add($this->courseRow->label);
+      $this['courseForm']->setDefaults($this->courseRow);
+    } catch (AbortException $e) {
+
+    }
+  }
+
+  public function renderView (int $id): void
+  {
+    $this->template->course = $this->courseRow;
+  }
+
+  public function actionAll (): void
+  {
+    try {
+      $this->guestRedirect();
+      $this['breadcrumb']->add('Kurzy', 'Courses:all');
+    } catch (AbortException $e) {
+
+    }
+  }
+
+  public function renderAll (): void
+  {
+    $this->template->courses = $this->coursesRepository->findAll();
+  }
+
+  public function __construct(AlbumsRepository $albumsRepository, SectionsRepository $sectionRepository, BreadcrumbControl $breadcrumbControl, SearchFormFactory $searchForm, CoursesRepository $coursesRepository, CourseFormFactory $courseFormFactory)
+  {
+    parent::__construct($albumsRepository, $sectionRepository, $breadcrumbControl, $searchForm);
+    $this->coursesRepository = $coursesRepository;
+    $this->courseFormFactory = $courseFormFactory;
+  }
+
+  protected function createComponentCourseForm (): Form
+  {
+    return $this->courseFormFactory->create(function (Form $form, ArrayHash $values) {
+      $this->getParameter('id') ? $this->submittedEditForm($values) : $this->submittedAddForm($values);
+    });
+  }
+
+  public function submittedAddForm (ArrayHash $values): void
+  {
+    try {
+      $this->guestRedirect();
+      $this->coursesRepository->insert($values);
+    } catch (AbortException $e) {
+
+    }
+  }
+
+  public function submittedEditForm (ArrayHash $values): void
+  {
+    try {
+      $this->guestRedirect();
+      $this->courseRow->update($values);
+    } catch (AbortException $e) {
+
+    }
+  }
 }
