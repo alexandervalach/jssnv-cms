3ed50056638967b59a145c3f81c60750e2629926
Alexander Valach <alexander.valach@gmail.com>
2020-06-11 19:24:50 +0200

    Added application form screen

--- app/forms/ApplicationFormFactory.php
+++ app/forms/ApplicationFormFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Forms;
 
 use App\Helpers\FormHelper;
+use App\Model\BranchClassesRepository;
 use Nette\Application\UI\Form;
 use Nette\SmartObject;
 use Nette\Utils\ArrayHash;
@@ -13,15 +14,41 @@ class ApplicationFormFactory
 {
   use SmartObject;
 
+  const CONSENT_NAME = 'Súhlasím so zverejnením svojho mena, priezviska a akademického titulu
+   na zozname poslucháčov prevádzkovateľa, ktorý bude zverejnený v septembri 2019
+   na www stránke školy za účelom informovania o prvom stretnutí a čase konania kurzov.';
+
+  const CONSENT_PHOTO = 'Súhlasím so zverejňovaním svojich fotografií/
+    fotografií svojej dcéry/syna/videoprodukcie na účely dokumentujúce a 
+    propagujúce činnosť prevádzkovateľa, zverejnenia týchto fotografií 
+    na www stránke prevádzkovateľa, nástenke prevádzkovateľa a 
+    v publikáciách vydávaných prevádzkovateľom. 
+    Doba trvania súhlasu platí, pokiaľ trvá účel ich spracovania. 
+    Dovtedy ho možno kedykoľvek písomne alebo elektronicky odvolať.';
+
+  const CONSENT_PERSONAL_DATA = 'Súhlas so spracovaním osobných údajov pre Jazykovú školu, 
+    Javorová 16, Spišská Nová Ves,
+    IČO: 35538791 (ďalej len „prevádzkovateľ“)
+    Súhlas so spracovaním osobných údajov v zmysle čl. 6 ods. 1 písm. 
+    a) Nariadenia EP a Rady EÚ č. 2016/679 o ochrane fyzických osôb 
+    pri spracúvaní osobných údajov a o voľnom pohybe takýchto údajov, 
+    ktorým sa zrušuje Smernica 95/46/ES 
+    (Všeobecné nariadenie o ochrane údajov, ďalej len „Nariadenie GDPR“).';
+
   /** @var FormFactory */
   private $formFactory;
 
+  /** @var BranchClassesRepository */
+  private $branchClassesRepository;
+
   /**
    * @param FormFactory $factory
+   * @param BranchClassesRepository $branchClassesRepository
    */
-  public function __construct(FormFactory $factory)
+  public function __construct(FormFactory $factory, BranchClassesRepository $branchClassesRepository)
   {
     $this->formFactory = $factory;
+    $this->branchClassesRepository = $branchClassesRepository;
   }
 
   /**
@@ -29,15 +56,88 @@ class ApplicationFormFactory
    * @param callable $onSuccess
    * @return Form
    */
-  public function create(callable $onSuccess): Form
+  public function create(int $branchId, callable $onSuccess): Form
   {
     $form = $this->formFactory->create();
 
-    $form->addText('label', 'Názov*')
-      ->setHtmlAttribute('placeholder','Zo života školy')
+    $currentYear = date('Y');
+    $lastYear =  date('Y',strtotime('-1 year'));
+
+    $form->addText('name', 'Meno a priezvisko')
+      ->setHtmlAttribute('placeholder','Arnošt Kábel')
       ->setRequired()
-      ->addRule(Form::MAX_LENGTH, 'Názov môže mať maximálne 255 znakov.', 255);
+      ->addRule(Form::MAX_LENGTH, '%label môže mať maximálne %value znakov', 255);
+
+    $form->addText('title_bn', 'Titul pred menom')
+      ->addRule(Form::MAX_LENGTH, '%label môže mať maximálne %value znakov', 50);
+
+    $form->addText('title_an', 'Titul za menom')
+      ->addRule(Form::MAX_LENGTH, '%label môže mať maximálne %value znakov', 50);
+
+    $form->addText('street_address', 'Adresa trvalého bydliska')
+      ->setHtmlAttribute('placeholder','Námestie slobody 1')
+      ->addRule(Form::MAX_LENGTH, '%label môže mať maximálne %value znakov', 255)
+      ->setRequired();
+
+    $form->addText('city', 'Mesto')
+      ->setHtmlAttribute('placeholder','Spišská Nová Ves')
+      ->addRule(Form::MAX_LENGTH, '%label môže mať maximálne %value znakov', 255)
+      ->setRequired();
+
+    $form->addText('zipcode', 'PSČ')
+      ->setHtmlAttribute('placeholder','Spišská Nová Ves')
+      ->addRule(Form::MAX_LENGTH, '%label môže mať maximálne %value znakov', 10)
+      ->setRequired();
+
+    // TODO: Check date HTML attribute
+    $form->addText('birthdate', 'Dátum narodenia')
+      ->setHtmlAttribute('type', 'date')
+      ->setHtmlAttribute('placeholder','2000-10-15')
+      ->setRequired();
+
+    $form->addText('birthplace', 'Miesto narodenia')
+      ->setHtmlAttribute('placeholder','Spišská Nová Ves')
+      ->addRule(Form::MAX_LENGTH, '%label môže mať maximálne %value znakov', 255)
+      ->setRequired();
+
+    $form->addText('id_number', 'Rodné číslo')
+      ->addRule(Form::MAX_LENGTH, '%label môže mať maximálne %value znakov', 15)
+      ->setRequired();
+
+    $form->addText('nationality', 'Národnosť')
+      ->addRule(Form::MAX_LENGTH, '%label môže mať maximálne %value znakov', 50)
+      ->setRequired();
+
+    $form->addText('email', 'E-mail')
+      ->setHtmlAttribute('type', 'email')
+      ->addRule(Form::MAX_LENGTH, '%label môže mať maximálne %value znakov', 50)
+      ->setRequired();
+
+    $form->addText('phone', 'Telefón')
+      ->addRule(Form::MAX_LENGTH, '%label môže mať maximálne %value znakov', 50)
+      ->setRequired();
+
+    $form->addText('employment', 'Zamestnanie (žiaci a študenti: škola, ročník)')
+      ->addRule(Form::MAX_LENGTH, '%label môže mať maximálne %value znakov', 255)
+      ->setRequired();
+
+    $form->addText('prev_course', 'Absolvovaný kurz na JŠ v šk. roku ' . $lastYear . '/' . $currentYear)
+      ->addRule(Form::MAX_LENGTH, '%label môže mať maximálne %value znakov', 255);
+
+    $form->addCheckbox('consent_personal_data', self::CONSENT_PERSONAL_DATA)
+      ->addRule(Form::MAX_LENGTH, '%label môže mať maximálne %value znakov', 255)
+      ->setRequired();
+
+    $form->addCheckbox('consent_name', self::CONSENT_NAME)
+      ->addRule(Form::MAX_LENGTH, '%label môže mať maximálne %value znakov', 255)
+      ->setRequired();
+
+    $form->addCheckbox('consent_photo', self::CONSENT_PHOTO)
+      ->addRule(Form::MAX_LENGTH, '%label môže mať maximálne %value znakov', 255)
+      ->setRequired();
+
     $form->addSubmit('save', 'Uložiť');
+
     $form->addSubmit('cancel', 'Zrušiť')
       ->setHtmlAttribute('class', 'btn btn-warning')
       ->setHtmlAttribute('data-dismiss', 'modal');
--- app/presenters/ApplicationFormsPresenter.php
+++ app/presenters/ApplicationFormsPresenter.php
@@ -5,21 +5,57 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use App\Components\BreadcrumbControl;
+use App\Forms\ApplicationFormFactory;
 use App\Forms\SearchFormFactory;
 use App\Model\AlbumsRepository;
+use App\Model\BranchesRepository;
 use App\Model\SectionsRepository;
 
 class ApplicationFormsPresenter extends BasePresenter
 {
-  public function __construct(AlbumsRepository $albumsRepository, SectionsRepository $sectionRepository, BreadcrumbControl $breadcrumbControl, SearchFormFactory $searchForm)
+  /**
+   * @var BranchesRepository
+   */
+  private $branchesRepository;
+
+  /**
+   * @var mixed|\Nette\Database\Table\ActiveRow|null
+   */
+  private $branchRow;
+
+  public function __construct(AlbumsRepository $albumsRepository, SectionsRepository $sectionRepository, BreadcrumbControl $breadcrumbControl, SearchFormFactory $searchForm, BranchesRepository $branchesRepository)
   {
     parent::__construct($albumsRepository, $sectionRepository, $breadcrumbControl, $searchForm);
+    $this->branchesRepository = $branchesRepository;
   }
 
-  public function actionAdd () {
+  public function actionAll (): void
+  {
 
   }
 
-  public function renderAdd () {
+  public function renderAll (): void
+  {
+    $this['breadcrumb']->add('Prihlášky');
+    $this->template->branches = $this->branchesRepository->findAll();
   }
+
+  public function actionAdd (int $id): void
+  {
+    $this->branchRow = $this->branchesRepository->findById($id);
+  }
+
+  public function renderAdd (int $id): void
+  {
+    $this['breadcrumb']->add('Prihlášky', $this->link('ApplicationForms:all'));
+    $this['breadcrumb']->add($this->branchRow->label);
+    // $this->template->branches = $this->branchesRepository->findAll();
+  }
+
+  /*
+  protected function createComponentApplicationForm (): Form
+  {
+    return $this->applicationFormFactory->create()
+  }
+  */
 }
